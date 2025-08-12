5d8072aa5e495612ea4bf0e510dba400618134df
vozexpertadm-ship-it <vozexpertadm@gmail.com>
2025-08-11 19:34:05 -0600

    Update mini.js

--- PaginaVozExperta/mini.js
+++ PaginaVozExperta/mini.js
@@ -4,6 +4,4 @@ const app = express();
 
 app.get('/ping', (_req, res) => res.send('pong'));
 app.get('/', (_req, res) => res.send('OK-mini'));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => console.log(`MINI listening on ${PORT}`));
+const PORT = process.env.PORT || 3000; app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
